b5cb8f7c4e2ee5c9038db8a1e75d3647ae19d431
Kaleb Franken <kaleb.franken7@gmail.com>
2023-01-22 18:51:47 -0700

    new files

--- nextjs13demo/app/Header.tsx
+++ nextjs13demo/app/Header.tsx
@@ -1,9 +1,10 @@
-
+import Link from "next/link"
 
 export default function Header() {
 	return (
 		<header className="p-5 bg-yellow-100">
 			<p>Header</p> 
+			<Link href="/" className="px-2 py-1 bg-white font-bold rounded-lg">Home</Link>
 		</header>
 	)
 }
--- nextjs13demo/app/todos/page.tsx
+++ nextjs13demo/app/todos/page.tsx
@@ -3,6 +3,6 @@ export default function Todos() {
 
 
 	return (<div>
-		
+		<h1>Todos</h1>
 	</div>)
 }
