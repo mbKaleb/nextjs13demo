9ff0727870c85da84ec189a2cf0a6c50e86601ae
Kaleb Franken <kaleb.franken7@gmail.com>
2023-01-26 10:04:00 -0700

    + dynamic route, + components

---
+++ nextjs13demo/app/todos/[todoId]/page.tsx
@@ -0,0 +1,43 @@
+import { Todo } from "../TodosList"
+
+type PageProps = {
+    params: {
+        todoId: string
+    }
+}
+
+const fetchTodos = async (todoId:string) => {
+
+    const res = await fetch( 
+        `https://jsonplaceholder.typicode.com/todos/${todoId}`, { next: {revalidate: 60} }
+        )
+    const todo: Todo = await res.json()
+    return todo
+    }
+
+export default async function TodoPage({params: { todoId }}: PageProps) {
+    const todo = await fetchTodos(todoId);
+
+  return (
+    <div>
+        <p>
+            #{todo.id}: {todo.title}
+        </p>
+        <p>Completed: {todo.completed ? "Yes": "No"}</p>
+        <p>
+            By User: {todo.userId}
+        </p>
+    </div>
+  )
+}
+
+export async function generateStaticParams( ) {
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos/")
+    const todos: Todo[] = await res.json()
+
+    const sTodos = todos.splice(0, 10)
+
+    return sTodos.map(todo => ({
+        todoId: todo.id.toString()
+    }))
+}
