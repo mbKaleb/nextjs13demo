7e0f939bd0240338c3c9a819cfd076607ad981a4
Kaleb Franken <kaleb.franken7@gmail.com>
2023-02-01 12:46:32 -0700

    commit

--- nextjs13demo/app/page.tsx
+++ nextjs13demo/app/page.tsx
@@ -1,8 +1,9 @@
 
 export default function Home() {
 	return (
-		<div className="text-red-600 bg-yellow-200 h-[93vh]">
-			
+		<div className="text-black bg-yellow-200 h-[93vh]">
+			The
+			Cat
 		</div>
 	)
 }
---
+++ nextjs13demo/app/textBackground.tsx
@@ -0,0 +1,6 @@
+export default function textBackground() {
+  return (
+    <div>textBackground</div>
+  )
+}
+
