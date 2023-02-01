446bb4e5fedb4abe1f0dea5bee6ee9ececf1c4a2
Kaleb Franken <kaleb.franken7@gmail.com>
2023-01-31 17:36:24 -0700

    style changes

--- nextjs13demo/app/Header.tsx
+++ nextjs13demo/app/Header.tsx
@@ -2,8 +2,8 @@ import Link from "next/link"
 
 export default function Header() {
 	return (
-		<header className="p-5 bg-yellow-100">
-			<p>Header</p> 
+		<header className="p-5 bg-black">
+			<p>amazonbasics</p>
 			<Link href="/" className="px-2 py-1 bg-white font-bold rounded-lg">Home</Link>
 		</header>
 	)
--- nextjs13demo/app/page.tsx
+++ nextjs13demo/app/page.tsx
@@ -1,6 +1,6 @@
 
 export default function Home() {
 	return (
-		<div className="text-red-600"> HOME </div>
+		<div className="text-red-600">  </div>
 	)
 }
