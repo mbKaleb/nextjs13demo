84dedea55ee797794e1072fceefc3426cfbb33e9
Kaleb Franken <kaleb.franken7@gmail.com>
2023-02-01 12:31:50 -0700

    +logo, positioned and looks good

--- nextjs13demo/app/Header.tsx
+++ nextjs13demo/app/Header.tsx
@@ -1,10 +1,11 @@
+import Image from "next/image"
 import Link from "next/link"
 
 export default function Header() {
 	return (
-		<header className="p-5 bg-black">
-			<p>amazonbasics</p>
-			<Link href="/" className="px-2 py-1 bg-white font-bold rounded-lg">Home</Link>
+		<header className="flex justify-center h-24 p-5 bg-black">
+			<Image className="margin-left-auto -top-10 absolute" src="/../public/amazon-basics-logo.png" alt="amazon basics logo" width={'200'} height={'5'} />
+			{/* <Link href="/" className="px-2 py-1 bg-white font-bold rounded-lg">Home</Link> */}
 		</header>
 	)
 }
--- nextjs13demo/app/page.tsx
+++ nextjs13demo/app/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
 	return (
-		<div className="text-red-600">
+		<div className="text-red-600 bg-yellow-100 h-[93vh]">
 			
 		</div>
 	)
