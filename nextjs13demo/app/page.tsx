04e32ef4b8cbb3b8dec6e37d35da26345546bfbe
Kaleb Franken <kaleb.franken7@gmail.com>
2023-02-18 08:43:45 -0700

    fixed compliation issue

--- nextjs13demo/app/page.tsx
+++ nextjs13demo/app/page.tsx
@@ -5,7 +5,6 @@ import { createEditor } from "slate"
 import { Slate, Editable, withReact } from "slate-react"
 
 export default function Home() {
-	console.log(document.body.scrollHeight)
 		// Create a Slate editor object that won't change across renders.
 		const [editor] = useState(() => withReact(createEditor()))
 		const initialValue = [
